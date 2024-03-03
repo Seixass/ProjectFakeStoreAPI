74b3e67ec04f70ec899039252eaeb1c6d67d256a
seixass <138785505+seixass@users.noreply.github.com>
2024-03-03 18:50:54 -0300

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,42 +1,44 @@
-// Função para realizar o login do usuário
+
 function login() {
-  // Obter os valores dos campos de entrada de usuário e senha
+
   var username = document.getElementById("loginUsername").value;
   var password = document.getElementById("loginPassword").value;
-  // Buscar o usuário salvo no localStorage com base no nome de usuário inserido
+
   var savedUser = localStorage.getItem(username);
-  // Verificar se o usuário existe no localStorage
+
   if (savedUser !== null) {
-    // Se o usuário existir, analisar os dados salvos em formato JSON
+
     var user = JSON.parse(savedUser);
-    // Verificar se a senha inserida corresponde à senha do usuário
+
     if (user.password === password) {
-      alert("Login bem sucedido!"); // Mostrar mensagem de login bem-sucedido
+      alert("Login bem sucedido!");
+      window.location.href = "../index.html"; // Redirecionar para a página principal após o login bem-sucedido
     } else {
-      alert("Senha incorreta!"); // Mostrar mensagem de senha incorreta
-    }
+
+      var loginBox = document.querySelector('.login-box');
+      loginBox.innerHTML = "<p>Senha incorreta!</p>";
+    }    
   } else {
-    alert("Usuário não encontrado!"); // Mostrar mensagem de usuário não encontrado
+    alert("Usuário não encontrado!"); 
   }
 }
 
-// Função para registrar um novo usuário
+
 function register() {
-  // Obter os valores dos campos de entrada de usuário e senha
+
   var username = document.getElementById("registerUsername").value;
   var password = document.getElementById("registerPassword").value;
-  // Verificar se o usuário já existe no localStorage
   var existingUser = localStorage.getItem(username);
+
   if (existingUser !== null) {
-    alert("Usuário já existe!"); // Mostrar mensagem de usuário já existente
   } else {
-    // Se o usuário não existe, criar um novo objeto de usuário
     var newUser = {
       username: username,
       password: password
     };
     // Salvar o novo usuário no localStorage
     localStorage.setItem(username, JSON.stringify(newUser));
-    alert("Registro bem sucedido!"); // Mostrar mensagem de registro bem-sucedido
+    alert("Registro bem sucedido!");
+    window.location.href = "../index.html"; // Redirecionar para a página principal após o registro bem-sucedido    
   }
 }
--- js/product.js
+++ js/product.js
@@ -1,35 +1,26 @@
-// Evento que é disparado quando o DOM está completamente carregado
 document.addEventListener("DOMContentLoaded", function () {
-  // Recuperar o ID do produto da URL
   const urlParams = new URLSearchParams(window.location.search);
   const productId = urlParams.get('id');
   
-  // Função para buscar os detalhes do produto pelo ID
   async function fetchProductDetails(productId) {
     try {
-      // Fazer uma requisição para obter os detalhes do produto usando o ID
       const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
-      // Converter a resposta para JSON
       const product = await response.json();
-      // Obter o contêiner onde os detalhes do produto serão exibidos
       const productDetailsContainer = document.querySelector('.product-details-container');
       
-      // Atualizar o HTML com os detalhes do produto
       productDetailsContainer.innerHTML = `
         <img class="product-image" src="${product.image}" alt="${product.title}">
         <div class="product-details">
           <h2 class="product-title">${product.title}</h2>
           <p class="product-description">${product.description}</p>
           <p class="product-price">Price: $${product.price}</p>
-          <button class="buy-button">Buy Now</button>
+          <button class="buy-button"><a href="./pagebuy.html">Buy Now</a></button>
         </div>
       `;
     } catch (error) {
-      // Lidar com erros caso a requisição falhe
       console.error('Error fetching product details:', error);
     }
   }
   
-  // Chamar a função para buscar os detalhes do produto
   fetchProductDetails(productId);
 });
